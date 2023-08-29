caff4b2d999b6c26a10fd1114cb1fa440458e80e
Hang Chen <hanglearning@gmail.com>
2023-08-28 20:24:17 -0400

    bug fix for output_length for pretrain.py

--- pretrain.py
+++ pretrain.py
@@ -64,7 +64,7 @@ print(f'We have {len(all_device_files)} devices available.')
 # init first model
 
 create_model = create_lstm if args["model"] == 'lstm' else create_gru
-model_units = [args['input_length'], args['hidden_neurons'], args['hidden_neurons'], 1]
+model_units = [args['input_length'], args['hidden_neurons'], args['hidden_neurons'], args['output_length']]
 model_configs = [args['loss'], args['optimizer'], args['metrics']]
 global_model_0 = create_model(model_units, model_configs)
 
